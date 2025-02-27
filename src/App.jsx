36aef51630ca02609c6c6d018e75f42ee413e630
iplesples <ceo.iplesples@gmail.com>
2025-02-27 15:11:55 +0700

    install react + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen w-screen bg-blue-400 text-5xl p-52'>
+        hallo gue bobhy, welcome to my site
+      </div>
+    </>
+  )
+}
+
+export default App
