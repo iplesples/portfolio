9d17d53f43f492478dea3c34d84d8af1d21283e3
iplesples <ceo.iplesples@gmail.com>
2025-02-27 15:23:38 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ function App() {
   return (
     <>
       <div className='h-screen w-screen bg-blue-400 text-5xl p-52'>
-        hallo gue bobhy, welcome to my site
+        <h1>hallo gue bobhy, welcome to my site</h1>
+        <h1 className='mt-3'>beli koin murah</h1>
+        <button className='bg-amber-400 p-4 hover:bg-amber-950'>klik disini</button>
       </div>
+      
     </>
   )
 }
