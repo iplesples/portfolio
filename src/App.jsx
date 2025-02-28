bcf36611164b12320a85221297195cadfe919cca
iplesples <ceo.iplesples@gmail.com>
2025-02-28 17:07:20 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AdminDashboard from './pages/AdminDashboard'
+
 import Home from './pages/Home'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   )
